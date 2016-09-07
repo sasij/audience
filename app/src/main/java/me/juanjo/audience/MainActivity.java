ef5a184eb974ab7d0c943aeaa3a8941f505907c2
Juanjo <juanjose.sanchezsilos@betvictor.com>
2016-09-07 16:20:29 +0200

    Record sound in background.

--- app/src/main/java/me/juanjo/audience/MainActivity.java
+++ app/src/main/java/me/juanjo/audience/MainActivity.java
@@ -30,163 +30,176 @@ import me.juanjo.audience.infraestructure.service.RecordService;
  */
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-   @BindView(R.id.record_button) Button recordButton;
-   @BindView(R.id.stop_record_button) Button stopRecordButton;
-   @BindView(R.id.play_sound_button) Button playButton;
+  private static final String LOG_TAG = "AudioRecordTest";
 
-   private static final String LOG_TAG = "AudioRecordTest";
+  @BindView(R.id.record_button) Button recordButton;
+  @BindView(R.id.stop_record_button) Button stopRecordButton;
+  @BindView(R.id.play_sound_button) Button playButton;
 
-   private MediaPlayer mPlayer = null;
+  /** Flag indicating whether we have called bind on the service. */
+  boolean mBound;
+  private MediaPlayer mPlayer = null;
+  private boolean recording;
+  private boolean playing;
+  /**
+   * Class for interacting with the main interface of the service.
+   */
+  private ServiceConnection mConnection = new ServiceConnection() {
+    public void onServiceConnected(ComponentName className, IBinder service) {
+      // This is called when the connection with the service has been
+      // established, giving us the object we can use to
+      // interact with the service.  We are communicating with the
+      // service using a Messenger, so here we get a client-side
+      // representation of that from the raw IBinder object.
+      mBound = true;
+    }
 
-   private boolean recording;
-   private boolean playing;
+    public void onServiceDisconnected(ComponentName className) {
+      // This is called when the connection with the service has been
+      // unexpectedly disconnected -- that is, its process crashed.
+      mBound = false;
+    }
+  };
 
-   /** Flag indicating whether we have called bind on the service. */
-   boolean mBound;
+  @Override public void onCreate(Bundle icicle) {
+    super.onCreate(icicle);
+    setContentView(R.layout.activity_main);
+    ButterKnife.bind(this);
 
-   /**
-    * Class for interacting with the main interface of the service.
-    */
-   private ServiceConnection mConnection = new ServiceConnection() {
-      public void onServiceConnected(ComponentName className, IBinder service) {
-         // This is called when the connection with the service has been
-         // established, giving us the object we can use to
-         // interact with the service.  We are communicating with the
-         // service using a Messenger, so here we get a client-side
-         // representation of that from the raw IBinder object.
-         mBound = true;
+    if (requestPermissionOnClick()) return;
+
+    recordButton.setOnClickListener(this);
+    stopRecordButton.setOnClickListener(this);
+    playButton.setOnClickListener(this);
+
+    initService();
+
+    final Button multiple = (Button) findViewById(R.id.button_test);
+    multiple.setOnClickListener(new View.OnClickListener() {
+      @Override public void onClick(View view) {
+        initService();
       }
+    });
+  }
 
-      public void onServiceDisconnected(ComponentName className) {
-         // This is called when the connection with the service has been
-         // unexpectedly disconnected -- that is, its process crashed.
-         mBound = false;
-      }
-   };
+  @Override protected void onStart() {
+    super.onStart();
+    // Bind to the service
+    bindService(new Intent(this, RecordService.class), mConnection, Context.BIND_AUTO_CREATE);
+  }
 
-   @Override public void onCreate(Bundle icicle) {
-      super.onCreate(icicle);
-      setContentView(R.layout.activity_main);
-      ButterKnife.bind(this);
+  @Override public void onPause() {
+    super.onPause();
 
-      if (requestPermissionOnClick()) return;
+    if (mPlayer != null) {
+      mPlayer.release();
+      mPlayer = null;
+    }
+  }
 
-      recordButton.setOnClickListener(this);
-      stopRecordButton.setOnClickListener(this);
-      playButton.setOnClickListener(this);
+  @Override protected void onStop() {
+    super.onStop();
 
-      initService();
+    // Unbind from the service
+    if (mBound) {
+      unbindService(mConnection);
+      mBound = false;
+    }
+  }
 
-      final Button multiple = (Button) findViewById(R.id.button_test);
-      multiple.setOnClickListener(new View.OnClickListener() {
-         @Override public void onClick(View view) {
-            initService();
-         }
-      });
-   }
+  @Override public void onClick(View v) {
+    switch (v.getId()) {
+      case R.id.record_button:
+        recording = true;
+        startRecording();
+        break;
+      case R.id.stop_record_button:
+        if (recording) {
+          recording = false;
+          stopRecording();
+        } else if (playing) {
+          playing = false;
+          stopPlaying();
+        }
+        break;
+      case R.id.play_sound_button:
+        playing = true;
+        startPlaying();
+        break;
+    }
+  }
 
-   @Override protected void onStart() {
-      super.onStart();
-      // Bind to the service
-      bindService(new Intent(this, RecordService.class), mConnection, Context.BIND_AUTO_CREATE);
-   }
+  private void startPlaying() {
+    mPlayer = new MediaPlayer();
+    try {
+      String fileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+      fileName += "/audiorecordtest.mp4";
+      mPlayer.setDataSource(fileName);
+      mPlayer.prepare();
+      mPlayer.start();
+    } catch (IOException e) {
+      Log.e(LOG_TAG, "prepare() failed");
+    }
+  }
 
-   @Override public void onPause() {
-      super.onPause();
+  private void stopPlaying() {
+    if (mPlayer != null) {
+      mPlayer.release();
+      mPlayer = null;
+    }
+  }
 
-      if (mPlayer != null) {
-         mPlayer.release();
-         mPlayer = null;
-      }
-   }
-
-   @Override protected void onStop() {
-      super.onStop();
-
-      // Unbind from the service
-      if (mBound) {
-         unbindService(mConnection);
-         mBound = false;
-      }
-   }
-
-   @Override public void onClick(View v) {
-      switch (v.getId()) {
-         case R.id.record_button:
-            break;
-         case R.id.stop_record_button:
-            if (recording) {
-            } else if (playing) {
-               playing = false;
-               stopPlaying();
-            }
-            break;
-         case R.id.play_sound_button:
-            playing = true;
-            startPlaying();
-            break;
-      }
-   }
-
-   private void startPlaying() {
-      mPlayer = new MediaPlayer();
-      try {
-         String fileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-         fileName += "/audiorecordtest.mp4";
-         mPlayer.setDataSource(fileName);
-         mPlayer.prepare();
-         mPlayer.start();
-      } catch (IOException e) {
-         Log.e(LOG_TAG, "prepare() failed");
-      }
-   }
-
-   private void stopPlaying() {
-      if (mPlayer != null) {
-         mPlayer.release();
-         mPlayer = null;
-      }
-   }
-
-   private boolean requestPermissionOnClick() {
-      //TODO test for request permission
-      //Move this to interactor => Check Permission
-      final Button multiple = (Button) findViewById(R.id.button_test);
-      multiple.setOnClickListener(new View.OnClickListener() {
-                                     @Override public void onClick(View v) {
-                                        if (Dexter.isRequestOngoing()) {
-                                           return;
+  private boolean requestPermissionOnClick() {
+    //TODO test for request permission
+    //Move this to interactor => Check Permission
+    final Button multiple = (Button) findViewById(R.id.button_test);
+    multiple.setOnClickListener(new View.OnClickListener() {
+                                  @Override public void onClick(View v) {
+                                    if (Dexter.isRequestOngoing()) {
+                                      return;
+                                    }
+                                    Dexter.checkPermissionsOnSameThread(new MultiplePermissionsListener() {
+                                      @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
+                                        System.out.println("=> onPermissionChecked" + report);
+                                        if (!report.areAllPermissionsGranted()) {
+                                          Toast.makeText(MainActivity.this,
+                                              "In order to use the application, check the permissions, please.",
+                                              Toast.LENGTH_SHORT).show();
                                         }
-                                        Dexter.checkPermissionsOnSameThread(new MultiplePermissionsListener() {
-                                           @Override public void onPermissionsChecked(MultiplePermissionsReport report) {
-                                              System.out.println("=> onPermissionChecked" + report);
-                                              if (!report.areAllPermissionsGranted()) {
-                                                 Toast.makeText(MainActivity.this,
-                                                     "In order to use the application, check the permissions, please.",
-                                                     Toast.LENGTH_SHORT).show();
-                                              }
-                                           }
+                                      }
 
-                                           @Override
-                                           public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions,
-                                               PermissionToken token) {
-                                              token.continuePermissionRequest();
-                                              System.out.println(
-                                                  "=> onPermissionRationaleShouldBeShown" + permissions.toString());
-                                           }
-                                        }, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO);
-                                     }
+                                      @Override
+                                      public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions,
+                                          PermissionToken token) {
+                                        token.continuePermissionRequest();
+                                        System.out.println("=> onPermissionRationaleShouldBeShown" + permissions.toString());
+                                      }
+                                    }, Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.RECORD_AUDIO);
                                   }
+                                }
 
-      );
-      return false;
-   }
+    );
+    return false;
+  }
 
-   private void initService() {
-      if (!mBound) return;
-      Intent intent = new Intent(getApplicationContext(), RecordService.class);
-      intent.setAction("INIT");
-      intent.putExtra("MY_USER_ID", "xxxdsasdcsdfc");
-      getApplicationContext().startService(intent);
-   }
+  private void initService() {
+    Intent intent = new Intent(getApplicationContext(), RecordService.class);
+    intent.setAction("INIT");
+    intent.putExtra("MY_USER_ID", "xxxdsasdcsdfc");
+    getApplicationContext().startService(intent);
+  }
+
+  private void startRecording() {
+    if (!mBound) return;
+    Intent intent = new Intent(getApplicationContext(), RecordService.class);
+    intent.setAction("RECORD");
+    getApplicationContext().startService(intent);
+  }
+
+  private void stopRecording() {
+    if (!mBound) return;
+    Intent intent = new Intent(getApplicationContext(), RecordService.class);
+    intent.setAction("STOP_RECORD");
+    getApplicationContext().startService(intent);
+  }
 }
--- app/src/main/java/me/juanjo/audience/infraestructure/service/RecordService.java
+++ app/src/main/java/me/juanjo/audience/infraestructure/service/RecordService.java
@@ -6,7 +6,6 @@ import android.media.MediaRecorder;
 import android.os.Binder;
 import android.os.Environment;
 import android.os.IBinder;
-import android.util.Log;
 import android.widget.Toast;
 import java.io.IOException;
 
@@ -15,81 +14,86 @@ import java.io.IOException;
  */
 public class RecordService extends Service {
 
-   // interface for clients that bind
-   private final IBinder mBinder = new RecordServiceBinder();
+  // interface for clients that bind
+  private final IBinder mBinder = new RecordServiceBinder();
 
-   private MediaRecorder mRecorder = null;
-   private boolean recording;
+  private MediaRecorder mRecorder = null;
+  private boolean recording;
 
-   public class RecordServiceBinder extends Binder {
-      public RecordService getService() {
-         return RecordService.this;
+  @Override public void onCreate() {
+    super.onCreate();
+    System.out.println("=> oncreate service");
+  }
+
+  @Override public int onStartCommand(Intent intent, int flags, int startId) {
+    if (intent != null) {
+      if ("INIT".equals(intent.getAction())) {
+        System.out.println("=> service was initialized");
+      } else if ("RECORD".equals(intent.getAction())) {
+        System.out.println("=> start recording service");
+        recording = true;
+        startRecording();
+      } else if ("STOP_RECORD".equals(intent.getAction())) {
+        System.out.println("=> stop recording service");
+        recording = false;
+        stopRecording();
       }
-   }
+    }
+    return START_STICKY;
+  }
 
-   @Override public void onCreate() {
-      super.onCreate();
-   }
+  /**
+   * When binding to the service, we return an interface to our messenger
+   * for sending messages to the service.
+   */
+  @Override public IBinder onBind(Intent intent) {
+    Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
+    return mBinder;
+  }
 
-   @Override public int onStartCommand(Intent intent, int flags, int startId) {
-      if (intent != null) {
-         if ("INIT".equals(intent.getAction())) {
-         } else if ("RECORD".equals(intent.getAction())) {
-            recording = true;
-            startRecording();
-         } else if ("STOP_RECORD".equals(intent.getAction())) {
-            recording = false;
-            stopRecording();
-         }
-      }
-      return START_STICKY;
-   }
+  @Override public void onRebind(Intent intent) {
+    super.onRebind(intent);
+    // A client is binding to the service with bindService(),
+    // after onUnbind() has already been called
+  }
 
-   /**
-    * When binding to the service, we return an interface to our messenger
-    * for sending messages to the service.
-    */
-   @Override public IBinder onBind(Intent intent) {
-      Toast.makeText(getApplicationContext(), "binding", Toast.LENGTH_SHORT).show();
-      return mBinder;
-   }
+  @Override public boolean onUnbind(Intent intent) {
+    System.out.println("=> unbind service ");
+    return super.onUnbind(intent);
+  }
 
-   @Override public void onRebind(Intent intent) {
-      super.onRebind(intent);
-      // A client is binding to the service with bindService(),
-      // after onUnbind() has already been called
-   }
+  @Override public void onDestroy() {
+    super.onDestroy();
+    System.out.println("=> ondestroy service ");
+  }
 
-   @Override public boolean onUnbind(Intent intent) {
-      return super.onUnbind(intent);
-   }
+  private void startRecording() {
+    String fileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+    fileName += "/audiorecordtest.mp4";
 
-   @Override public void onDestroy() {
-      super.onDestroy();
-   }
+    mRecorder = new MediaRecorder();
+    mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+    mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+    mRecorder.setOutputFile(fileName);
+    mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
 
-   private void startRecording() {
-      String fileName = Environment.getExternalStorageDirectory().getAbsolutePath();
-      fileName += "/audiorecordtest.mp4";
-
-      mRecorder = new MediaRecorder();
-      mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-      mRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
-      mRecorder.setOutputFile(fileName);
-      mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
-
-      try {
-         mRecorder.prepare();
-      } catch (IOException e) {
-         Log.e("RECORD_SERVICE", "prepare() failed");
-      }
-
+    try {
+      mRecorder.prepare();
       mRecorder.start();
-   }
+    } catch (IOException e) {
+      System.out.println("=> RECORD_SERVICE prepare() failed");
+    }
+  }
 
-   private void stopRecording() {
-      mRecorder.stop();
-      mRecorder.release();
-      mRecorder = null;
-   }
+  private void stopRecording() {
+    mRecorder.stop();
+    mRecorder.release();
+    mRecorder = null;
+  }
+
+  public class RecordServiceBinder extends Binder {
+    public RecordService getService() {
+      return RecordService.this;
+    }
+  }
 }
